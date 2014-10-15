7b9b3c0530b1c00c5bcb814bb48a7bb9621a0e36
zsp1987 <zsp1987@gmail.com>
2014-10-14 22:25:42 -0400

    LinkedListCycle

---
+++ src/leetcode/LinkedListCycle.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+/*
+ Given a linked list, determine if it has a cycle in it.
+
+ Follow up:
+ Can you solve it without using extra space? 
+ */
+public class LinkedListCycle {
+
+}
+
+class Solution {
+
+	class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
+
+	public boolean hasCycle(ListNode head) {
+
+		if (head == null)
+			return false;
+
+		ListNode fast = head;
+		ListNode slow = head;
+
+		if (fast.next != fast && fast.next != null)
+			fast = fast.next;
+
+		while (fast.next != null && fast.next.next != null) {
+			if (fast == slow)
+				return true;
+			fast = fast.next.next;
+			if (slow.next != null)
+				slow = slow.next;
+		}
+
+		return false;
+
+	}
+}
