6333404923be06ef0c0e9444bc90465dc835fa64
zsp1987 <zsp1987@gmail.com>
2014-12-26 20:22:39 -0500

    studied

---
+++ src/leetcode/FindPeakElement.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+/*
+ A peak element is an element that is greater than its neighbors.
+
+ Given an input array where num[i] ≠ num[i+1], find a peak element and return its index.
+
+ The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+
+ You may imagine that num[-1] = num[n] = -∞.
+
+ For example, in array [1, 2, 3, 1], 3 is a peak element and your function should return the index number 2.
+ */
+
+public class FindPeakElement {
+
+}
+
+class FindPeakElementSolution {
+	public int findPeakElement(int[] num) {
+		int n = num.length;
+		if (n == 0)
+			return 0;
+		if (n == 1)
+			return 0;
+		return findPeakAux(num, 0, n - 1);
+	}
+
+	int findPeakAux(int[] num, int s, int e) {
+		int mid = (s + e) / 2;
+		if (mid - 1 < s) {
+			if (s == e)
+				return s;
+			else
+				return num[s] > num[e] ? s : e;
+
+		}
+
+		if (num[mid] > num[mid - 1] && num[mid] > num[mid + 1])
+			return mid;
+		else if (num[mid] > num[mid - 1])
+			return findPeakAux(num, mid + 1, e);
+		else
+			return findPeakAux(num, s, mid - 1);
+	}
+}
---
+++ src/leetcode/GenerateParentheses.java
@@ -0,0 +1,58 @@
+package leetcode;
+
+/*
+ Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+ For example, given n = 3, a solution set is:
+
+ "((()))", "(()())", "(())()", "()(())", "()()()" 
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParentheses {
+
+}
+
+class GenerateParenthesesSolution {
+	public List<String> generateParenthesis(int n) {
+
+		char[] result = new char[2 * n];
+		int pos = 0;
+		int l = n;
+		int r = n;
+		List<String> resultList = new ArrayList<String>();
+		proc(result, pos, l, r, n, resultList);
+		return resultList;
+
+	}
+
+	void proc(char[] result, int pos, int l, int r, int n,
+			List<String> resultList) {
+		if (pos == 2 * n) {
+			String vaild = new String(result);
+			if (!resultList.contains(vaild)) {
+				resultList.add(vaild);
+			}
+		} else {
+			char[] l_result = result.clone();
+			char[] r_result = result.clone();
+
+			if (l != 0) {
+				l_result[pos] = '(';
+				proc(l_result, pos + 1, l - 1, r, n, resultList);
+			}
+
+			if (check(l, r)) {
+				r_result[pos] = ')';
+				proc(r_result, pos + 1, l, r - 1, n, resultList);
+			}
+
+		}
+	}
+
+	boolean check(int l, int r) {
+		return l < r ? true : false;
+	}
+}
