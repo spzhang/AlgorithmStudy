0b96d68e73fb83d9e78e59eeba375efa580d7fae
zsp1987 <zsp1987@gmail.com>
2015-01-08 21:57:13 -0500

    jan 8

--- src/leetcode/UniqueBinarySearchTrees.java
+++ src/leetcode/UniqueBinarySearchTrees.java
@@ -1,30 +1,31 @@
 package leetcode;
 
 /*
-Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
+ Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
 
-For example,
-Given n = 3, there are a total of 5 unique BST's.
+ For example,
+ Given n = 3, there are a total of 5 unique BST's.
  */
 
-public class UniqueBinarySearchTrees{
+public class UniqueBinarySearchTrees {
 }
 
 class UniqueBinarySearchTreesSolution {
-    public int numTrees(int n) {
-    
-        if(n==0) return 1;
-        if(n==1) return 1;
+	public int numTrees(int n) {
 
-        int sum=0;
-        
-        for(int i=0; i<=n-1; i++){
-            sum += numTrees(i)*numTrees(n-1-i);
-        }
-        
-        return sum;
-            
+		if (n == 0)
+			return 1;
+		if (n == 1)
+			return 1;
 
-    }
-    
+		int sum = 0;
+
+		for (int i = 0; i <= n - 1; i++) {
+			sum += numTrees(i) * numTrees(n - 1 - i);
+		}
+
+		return sum;
+
+	}
+
 };
--- src/leetcode/UniquePaths.java
+++ src/leetcode/UniquePaths.java
@@ -1,24 +1,32 @@
 package leetcode;
 
+/*
+ A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+ The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+ How many possible unique paths are there?
+ */
 public class UniquePaths {
 
 }
 
 class UniquePathsSolution {
-    public int uniquePaths(int m, int n) {
-        if(n<1 || m<1) return 0;
-        int[][] steps = new int[m][n];
-        for(int i = 0; i<m; i++){
-            steps[i][0]=1;
-        }
-        for(int j = 0; j<n; j++){
-            steps[0][j]=1;
-        }
-        for(int i=1; i<m;i++){
-            for(int j =1; j<n;j++){
-                steps[i][j] = steps[i-1][j]+steps[i][j-1];
-            }
-        }
-        return steps[m-1][n-1];
-    }
+	public int uniquePaths(int m, int n) {
+		if (n < 1 || m < 1)
+			return 0;
+		int[][] steps = new int[m][n];
+		for (int i = 0; i < m; i++) {
+			steps[i][0] = 1;
+		}
+		for (int j = 0; j < n; j++) {
+			steps[0][j] = 1;
+		}
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++) {
+				steps[i][j] = steps[i - 1][j] + steps[i][j - 1];
+			}
+		}
+		return steps[m - 1][n - 1];
+	}
 }
