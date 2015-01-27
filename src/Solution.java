62c53f3e188972f8a84a3738dfc430cb0c805f22
zsp1987 <zsp1987@gmail.com>
2015-01-27 09:46:26 -0500

    repeating

--- src/Solution.java
+++ src/Solution.java
@@ -1,9 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Solution {
 
 	public static void main(String[] args) {
+        List<List<String>> pre = new ArrayList<List<String>>();
+        List<List<String>> cur = new ArrayList<List<String>>();
+        for(List<String> list: pre){
+        	list.add("hello");
+        	cur.add(list);
+        }
 
-
+        System.out.println(cur);
 	}
+
 
 }
---
+++ src/leetcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,73 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+
+}
+
+class LongestSubstringWithoutRepeatingCharactersSolution {
+	public int lengthOfLongestSubstring(String s) {
+		HashMap<Character, Integer> map = new HashMap<>();
+		char[] ch = s.toCharArray();
+		int max = 0;
+		int count = 0;
+		for (int i = 0; i < ch.length; i++) {
+			count++;
+			if (map.containsKey(ch[i])) {
+				count = Math.min(count, i - map.get(ch[i]));
+			}
+			map.put(ch[i], i);
+			if (count > max)
+				max = count;
+		}
+		return max;
+
+	}
+
+}
+
+/*
+ * my solution will cause the time out
+ */
+class LongestSubstringWithoutRepeatingCharactersSolution2 {
+	public int lengthOfLongestSubstring(String s) {
+		if (s == null || s.length() == 0)
+			return 0;
+
+		int max = 0;
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+
+		int count = 0;
+		for (int i = 0; i < s.length(); i++) {
+			Character curChar = s.charAt(i);
+			if (!map.containsKey(curChar)) {
+				count++;
+				map.put(curChar, count);
+			} else {
+				max = count > max ? count : max;
+				int pos = map.get(curChar);
+				reset(map, pos);
+				count = count - pos;
+				count++;
+				map.put(curChar, count);
+			}
+		}
+
+		return max;
+
+	}
+
+	void reset(Map<Character, Integer> map, int n) {
+		for (Entry<Character, Integer> entry : map.entrySet()) {
+			if (entry.getValue() <= n) {
+				map.put(entry.getKey(), 0);
+			} else {
+				map.put(entry.getKey(), entry.getValue() - n);
+			}
+		}
+	}
+
+}
