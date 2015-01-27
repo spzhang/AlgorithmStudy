35d9cf75369217c95538210ab26151412b22e4b7
zsp1987 <zsp1987@gmail.com>
2015-01-27 14:34:29 -0500

    word

--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,37 @@
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Solution {
+	boolean result = false;
 
 	public static void main(String[] args) {
-        List<List<String>> pre = new ArrayList<List<String>>();
-        List<List<String>> cur = new ArrayList<List<String>>();
-        for(List<String> list: pre){
-        	list.add("hello");
-        	cur.add(list);
-        }
-
-        System.out.println(cur);
+		System.out.println("s".substring(1).length());
+		Solution s = new Solution();
+		String str = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab";
+		
+		String[] list = {"a", "aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"};
+		Set<String> dict = new HashSet<String>();
+		dict.addAll(Arrays.asList(list));
+		System.out.println(s.wordBreak(str, dict));
 	}
 
+	public boolean wordBreak(String s, Set<String> dict) {
+
+		if (s == null || dict == null)
+			return false;
+		if (s.length() == 0)
+			return true;
+
+		for (String str : dict) {
+			if (s.startsWith(str))
+				result = result || wordBreak(s.substring(str.length()), dict);
+			if (result == true)
+				break;
+		}
+
+		return result;
+
+	}
 
 }
---
+++ src/leetcode/WordBreak.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+/*
+ Given a string s and a dictionary of words dict, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+ For example, given
+ s = "leetcode",
+ dict = ["leet", "code"].
+
+ Return true because "leetcode" can be segmented as "leet code". 
+ */
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+public class WordBreak {
+
+}
+
+class WordBreakSolution {
+
+	public boolean wordBreak(String s, Set<String> dict) {
+		if (s == null || s.length() == 0)
+			return false;
+		else if (dict.contains(s))
+			return true;
+		
+		//the first letter that dict cannot build.
+		List<Integer> starts = new LinkedList<Integer>();
+		starts.add(0);
+		for (int end = 1; end <= s.length(); end++) {
+			for (int i = 0; i < starts.size(); i++) {
+				if (dict.contains(s.substring(starts.get(i), end))) {
+					starts.add(0, end);
+					break;
+				}
+			}
+		}
+		return (starts.get(0) == s.length());
+	}
+
+}
