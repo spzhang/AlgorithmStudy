ae9736f69023757761894782c5fb9f83acd79e4d
zsp1987 <zsp1987@gmail.com>
2015-02-07 11:32:26 -0500

    Manacher’s Algorithm

---
+++ src/leetcode/LongestPalindromicSubstring.java
@@ -0,0 +1,61 @@
+package leetcode;
+
+/*
+ (Manacher’s Algorithm)
+ Given a string S, find the longest palindromic substring in S.
+ You may assume that the maximum length of S is 1000,
+ and there exists one unique longest palindromic substring.
+ */
+
+public class LongestPalindromicSubstring {
+
+}
+
+class LongestPalindromicSubstringSolution {
+	String preProcess(String s) {
+		int n = s.length();
+		if (n == 0)
+			return "^$";
+		String ret = "^";
+		for (int i = 0; i < n; i++)
+			ret += "#" + s.substring(i, i + 1);
+
+		ret += "#$";
+		return ret;
+	}
+
+	String longestPalindrome(String s) {
+		String t = preProcess(s);
+		int n = t.length();
+		int[] P = new int[n];
+		int C = 0, R = 0;
+		for (int i = 1; i < n - 1; i++) {
+			int i_mirror = 2 * C - i; // equals to i' = C - (i-C)
+			P[i] = (R > i) ? Math.min(R - i, P[i_mirror]) : 0;
+
+			// Attempt to expand palindrome centered at i
+			while (t.charAt(i + 1 + P[i]) == t.charAt(i - 1 - P[i]))
+				P[i]++;
+
+			// If palindrome centered at i expand past R,
+			// adjust center based on expanded palindrome.
+			if (i + P[i] > R) {
+				C = i;
+				R = i + P[i];
+			}
+		}
+
+		// Find the maximum element in P.
+		int maxLen = 0;
+		int centerIndex = 0;
+		for (int i = 1; i < n - 1; i++) {
+			if (P[i] > maxLen) {
+				maxLen = P[i];
+				centerIndex = i;
+			}
+		}
+
+		return s.substring((centerIndex - 1 - maxLen) / 2,
+				(centerIndex - 1 - maxLen) / 2 + maxLen);
+	}
+}
