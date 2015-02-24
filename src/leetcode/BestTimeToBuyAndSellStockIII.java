4454809bdbe7a69dd8bfc63ca9f1db41dc2d6e8f
zsp1987 <zsp1987@gmail.com>
2015-02-24 16:10:31 -0500

    best time stock problem finished

--- src/leetcode/BestTimeToBuyAndSellStockIII.java
+++ src/leetcode/BestTimeToBuyAndSellStockIII.java
@@ -7,51 +7,27 @@ package leetcode;
 
  Note:
  You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+
+ global[i][j]=max(local[i][j],global[i-1][j])，
+ local[i][j]=max(global[i-1][j-1]+max(diff,0),local[i-1][j]+diff)，
+
  */
 
 public class BestTimeToBuyAndSellStockIII {
+
 	public int maxProfit(int[] prices) {
-		if (prices.length < 2)
-			return 0;// one of zero days, cannot sell
-		// break the problem in to subproblems, what is the max profit if i
-		// decide to buy and sell one stock on or before day i
-		// and the other stock after day i
-
-		int[] left = new int[prices.length];// store the max profit so far for day [0,i] for i from 0 to n
-		int[] right = new int[prices.length];// store the max profit so far for the days [i,n] for i from 0 to n
-		int minl, maxprofit, maxr, profit;
-
-		maxprofit = 0;// lower bound on profit
-		minl = Integer.MAX_VALUE;// minimum price so far for populating left array
-		for (int i = 0; i < left.length; i++) {
-			if (prices[i] < minl)
-				minl = prices[i];// check if this price is the minimum price so far
-			profit = prices[i] - minl;// get the profit of selling at current price having bought at min price so far
-			if (profit > maxprofit)
-				maxprofit = profit;// if the profit is greater than the profit so far, update the max profit
-			left[i] = maxprofit;
-		}
-
-		maxprofit = 0;// reset maxprofit to its lower bound
-		maxr = Integer.MIN_VALUE;// maximum price so far for populating the right array
-		// same line of reasoning as the above
-		for (int i = left.length - 1; i >= 0; i--) {
-			if (prices[i] > maxr)
-				maxr = prices[i];
-			profit = maxr - prices[i];
-			if (profit > maxprofit)
-				maxprofit = profit;
-			right[i] = maxprofit;
-		}
-		// get the best by combining the subproblems as described above
-		int best = 0;
+		if (prices == null || prices.length == 0)
+			return 0;
+		int[] local = new int[3];
+		int[] global = new int[3];
 		for (int i = 0; i < prices.length - 1; i++) {
-			if (left[i] + right[i + 1] > best)
-				best = left[i] + right[i + 1];
+			int diff = prices[i + 1] - prices[i];
+			for (int j = 2; j >= 1; j--) {
+				local[j] = Math.max(global[j - 1] + (diff > 0 ? diff : 0),
+						local[j] + diff);
+				global[j] = Math.max(local[j], global[j]);
+			}
 		}
-		best = best > maxprofit ? best : maxprofit;
-		// in total 3 passes required and 2 extra arrays of size n
-		return best;
-
+		return global[2];
 	}
 }
--- src/leetcode/BestTimeToBuyAndSellStockIV.java
+++ src/leetcode/BestTimeToBuyAndSellStockIV.java
@@ -1,22 +1,51 @@
 package leetcode;
 
 /*
-Say you have an array for which the ith element is the price of a given stock on day i.
+ Say you have an array for which the ith element is the price of a given stock on day i.
 
-Design an algorithm to find the maximum profit. You may complete at most k transactions.
+ Design an algorithm to find the maximum profit. You may complete at most k transactions.
 
-Note:
-You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+ Note:
+ You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
 
-Credits:
-Special thanks to @Freezen for adding this problem and creating all test cases.
+ Credits:
+ Special thanks to @Freezen for adding this problem and creating all test cases.
  */
 
 public class BestTimeToBuyAndSellStockIV {
 
-    public int maxProfit(int k, int[] prices) {
+	public int maxProfit(int k, int[] prices) {
+		if (prices.length == 0)
+			return 0;
+		if (k >= prices.length)
+			return solveMaxProfit(prices); // k is larger than operaitons
 
+		int[] local = new int[k + 1]; // before day i, at most j transcation,
+										// and last transcation at last day
+		int[] global = new int[k + 1];
 
-        return 0;
-    }
+		local[0] = 0;
+		global[0] = 0;
+		for (int i = 0; i < prices.length - 1; i++) {
+			int diff = prices[i + 1] - prices[i];
+			for (int j = k; j >= 1; --j) {
+				local[j] = Math.max(global[j - 1] + Math.max(diff, 0), local[j]
+						+ diff);
+				global[j] = Math.max(global[j], local[j]);
+			}
+		}
+
+		return global[k];
+
+	}
+
+	int solveMaxProfit(int[] prices) {
+		int ret = 0;
+		for (int i = 1; i < prices.length; i++) {
+			if (prices[i] - prices[i - 1] > 0) {
+				ret += prices[i] - prices[i - 1];
+			}
+		}
+		return ret;
+	}
 }
