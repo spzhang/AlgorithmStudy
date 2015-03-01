54dd5a6829afddcf648b07e93c87d72a7d67b5b9
zsp1987 <zsp1987@gmail.com>
2015-03-01 18:33:54 -0500

    easy and hard

---
+++ src/leetcode/CompareVersionNumbers.java
@@ -0,0 +1,56 @@
+package leetcode;
+
+/*
+ Compare two version numbers version1 and version2.
+ If version1 > version2 return 1, if version1 < version2 return -1, otherwise return 0.
+
+ You may assume that the version strings are non-empty and contain only digits and the . character.
+ The . character does not represent a decimal point and is used to separate number sequences.
+ For instance, 2.5 is not "two and a half" or "half way to version three", it is the fifth second-level revision of the second first-level revision.
+
+ Here is an example of version numbers ordering:
+
+ 0.1 < 1.1 < 1.2 < 13.37
+ */
+public class CompareVersionNumbers {
+
+	public static void main(String[] args) {
+		CompareVersionNumbers s = new CompareVersionNumbers();
+		System.out.println(s.compareVersion("12.0", "12"));
+	}
+
+	public int compareVersion(String version1, String version2) {
+		if (version1 == null || version2 == null)
+			return 0;
+		String[] v1 = version1.split("\\.");
+		String[] v2 = version2.split("\\.");
+		int l1 = v1.length - 1;
+		int l2 = v2.length - 1;
+
+		int a = l1;
+		int b = l2;
+		while (a >= 0 && b >= 0) {
+			if (Integer.parseInt(v1[l1 - a]) > Integer.parseInt(v2[l2 - b])) {
+				return 1;
+			} else if (Integer.parseInt(v1[l1 - a]) < Integer.parseInt(v2[l2
+					- b])) {
+				return -1;
+			} else {
+				a--;
+				b--;
+			}
+		}
+		while (a >= 0) {
+			if (Integer.parseInt(v1[l1 - a]) != 0)
+				return 1;
+			else a--;
+		}
+		while (b >= 0) {
+			if (Integer.parseInt(v2[l2 - b]) != 0)
+				return -1;
+			else b--;
+		}
+		return 0;
+
+	}
+}
---
+++ src/leetcode/WildcardMatching.java
@@ -0,0 +1,57 @@
+package leetcode;
+
+import com.sun.org.apache.xalan.internal.xsltc.compiler.Pattern;
+
+/*
+
+ Implement wildcard pattern matching with support for '?' and '*'.
+
+ '?' Matches any single character.
+ '*' Matches any sequence of characters (including the empty sequence).
+
+ The matching should cover the entire input string (not partial).
+
+ The function prototype should be:
+ bool isMatch(const char *s, const char *p)
+
+ Some examples:
+ isMatch("aa","a") → false
+ isMatch("aa","aa") → true
+ isMatch("aaa","aa") → false
+ isMatch("aa", "*") → true
+ isMatch("aa", "a*") → true
+ isMatch("ab", "?*") → true
+ isMatch("aab", "c*a*b") → false
+
+ */
+public class WildcardMatching {
+	public boolean isMatch(String s, String p) {
+		int si = 0, pi = 0, match = 0, startpi = -1;
+		while (si < s.length()) {
+			// ? or matching, increase both pointer
+			if (pi < p.length()
+					&& (p.charAt(pi) == '?' || s.charAt(si) == p.charAt(pi))) {
+				si++;
+				pi++;
+			}
+			// * found, only increase pattern pointer
+			else if (pi < p.length() && p.charAt(pi) == '*') {
+				startpi = pi;
+				match = si;
+				pi++;
+			}
+			// last pattern pointer was *, increase string pointer
+			else if (startpi != -1) {
+				pi = startpi + 1;
+				match++;
+				si = match;
+			}
+			// current pointer is not *, last pattern pointer war not *
+			else
+				return false;
+		}
+		while (pi < p.length() && p.charAt(pi) == '*')
+			pi++;
+		return pi == p.length();
+	}
+}
