a2d2995a984bf784ccf53b45f516c8b41f24ade7
zsp1987 <zsp1987@gmail.com>
2015-03-05 18:13:25 -0500

    today commmit

---
+++ src/algorithm/Tester.java
@@ -0,0 +1,7 @@
+package algorithm;
+public class Tester {
+	
+	public static void main(String[] args) {
+	}
+
+}
---
+++ src/leetcode/StringToInteger.java
@@ -0,0 +1,67 @@
+package leetcode;
+
+/*
+ Implement atoi to convert a string to an integer.
+
+ Hint: Carefully consider all possible input cases. If you want a challenge, please do not see below and ask yourself what are the possible input cases.
+
+ Notes: It is intended for this problem to be specified vaguely (ie, no given input specs). You are responsible to gather all the input requirements up front. 
+
+ Requirements for atoi:
+
+ The function first discards as many whitespace characters as necessary until the first non-whitespace character is found. Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as a numerical value.
+
+ The string can contain additional characters after those that form the integral number, which are ignored and have no effect on the behavior of this function.
+
+ If the first sequence of non-whitespace characters in str is not a valid integral number, or if no such sequence exists because either str is empty or it contains only whitespace characters, no conversion is performed.
+
+ If no valid conversion could be performed, a zero value is returned. If the correct value is out of the range of representable values, INT_MAX (2147483647) or INT_MIN (-2147483648) is returned.
+ */
+
+public class StringToInteger {
+	
+	public static void main(String[] args) {
+		StringToInteger s = new StringToInteger();
+		System.out.println(s.atoi("123"));
+	}
+
+	public int atoi(String str) {
+
+		if (str == null || str.length() < 1) {
+			return 0;
+		}
+
+		str = str.trim();
+
+		// check the sign
+		char flag = '+';
+		int i = 0;
+		if (str.charAt(0) == '-') {
+			flag = '-';
+			i++;
+		} else if (str.charAt(0) == '+') {
+			i++;
+		}
+
+		double ret = 0;
+
+		while (str.length() > i && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+
+			ret = ret * 10 + (str.charAt(i) - '0');
+			
+			i++;
+
+		}
+
+		if (flag == '-')
+			ret = -ret;
+
+		if (ret > Integer.MAX_VALUE)
+			return Integer.MAX_VALUE;
+		if (ret < Integer.MIN_VALUE)
+			return Integer.MIN_VALUE;
+
+		return (int) ret;
+	}
+
+}
